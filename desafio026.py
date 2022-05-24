2797c776b602ecc9564db99e4a69250befd04672
lpvasco <lucasp_vasco@hotmail.com>
2022-05-24 17:46:42 -0300

    python

--- desafio026.py
+++ desafio026.py
@@ -1,4 +1,4 @@
-n = str(input('Digite uma frase: ')).lower().strip()
-print('A letra A apareceu {} vezes.'.format(n.count('a')))
-print('A letra A apareceu pela primeira vez na posição {}'.format(n.find('a')+1))
-print('A letra A apareceu pela primeira vez na posição {}'.format(n.rfind('a')+1))
+frase = str(input('Digite uma frase: ')).lower().strip()
+print('A letra A apareceu {} vezes.'.format(frase.count('a')))
+print('A letra A apareceu pela primeira vez na posição {}'.format(frase.find('a')+1))
+print('A letra A apareceu pela primeira vez na posição {}'.format(frase.rfind('a')+1))
