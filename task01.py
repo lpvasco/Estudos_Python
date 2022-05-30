4109aebecf4b888ff9920d497da838e2402e59db
lpvasco <lucasp_vasco@hotmail.com>
2022-05-30 15:21:51 -0300

    python

--- task01.py
+++ task01.py
@@ -1,4 +1,7 @@
 n1 = int(input('Digite a nota: '))
 n2 = int(input('Digite a nota: '))
+print('=============================')
 print('Sua média nas 2 provas é {}'.format((n1+n2)/2))
+print('=============================')
+
 
