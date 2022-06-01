a4dfb20d29b82ebe30bf4bd4e110db6761c9c694
lpvasco <lucasp_vasco@hotmail.com>
2022-06-01 20:23:16 -0300

    python

--- desafio020.py
+++ desafio020.py
@@ -5,4 +5,6 @@ n3 = str(input('Digite o nome de um aluno: '))
 n4 = str(input('Digite o nome de um aluno: '))
 lista = [n1,n2,n3,n4]
 random.shuffle(lista)
+print('================================')
 print('A ordem de apresentação será: {}'.format(lista))
+print('================================')
